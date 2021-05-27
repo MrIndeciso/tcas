8fb33672e4290adcf0edf8a651c2fdbc8bd11ebf
Roberto Bertolini <robertobertolini37@gmail.com>
2021-05-27 20:49:59 +0200

    Final draft for tonight

--- src/tcas_cli.c
+++ src/tcas_cli.c
@@ -14,7 +14,9 @@ int main() {
     //char *test = "lim / * - ln + ln exp x ln ln exp x ln ln exp x ln exp x ln + ln exp x ln ln ln exp x x +infinity";
     //char *test = "lim / * - ln + x ln x ln x x ln + x ln ln x x +infinity";
     //char *test = "lim - ln ln + * x exp * x exp x 1 exp exp + ln ln x / 1 x x +infinity";
-    char *test = "lim / sin x x x 0";
+    //char *test = "lim / sin x x x 0";
+
+    char *test = "lim * / - ln + ln x ln ln x ln ln x ln + ln x ln ln ln x ln x x +infinity";
 
     size_t len = strlen(test);
     struct expr_tree_val *res = evaluate_simple_expr(len, test, settings);
